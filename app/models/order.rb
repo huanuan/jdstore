fe673f6260e0a7e8b3dfdc370265041a81f62958
Ping Xie <xieping7006@gmail.com>
2016-08-17 13:27:00 +0800

    gem aasm

--- app/models/order.rb
+++ app/models/order.rb
@@ -9,7 +9,7 @@ class Order < ApplicationRecord
 
   has_many :product_lists
 
-before_create :generate_token
+  before_create :generate_token
 
   def generate_token
     self.token = SecureRandom.uuid
@@ -21,6 +21,39 @@ before_create :generate_token
     else
       "未支付"
     end
+  end
+
+  include AASM
+
+  assm do
+    state :order_placed, initial: true
+    stste :paid
+    state :shopping
+    state :shoped
+    state :order_cancelled
+    state :good_returned
+
+    event :make_payment do
+      transitions from: :order_placed, to: :paid
+    end
+
+    event :ship do
+      translations from: :paid,         to: :shopping
+    end
+
+    event :deliver do
+      transitions from: :shopping,      to: :shopped
+    end
+
+    event :return_good do
+      transitions from: :shopped,       to: :good_returned
+    end
+
+    event :cancell_order do
+      transitions from: [:order_placed, :paid], to: :order_cancelled
+    end
+
+
   end
 
 end
